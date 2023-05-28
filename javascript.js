59f51eb3e46bfc1b03ad2b32a467540a6ad11ba6
Geoecho <43762958+Deafninja69@users.noreply.github.com>
2023-05-28 17:53:23 +0200

    Done

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// JavaScript for responsive menu
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.querySelector(".menu-icon");
+    const navUl = document.querySelector("nav ul");
+    
+    menuIcon.addEventListener("click", function() {
+      navUl.classList.toggle("show");
+    });
+  });
